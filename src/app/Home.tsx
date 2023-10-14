ee9c65a49ab0463afe47b1c769f826573f096586
christoph-codes <tkcwebdev@gmail.com>
2023-10-13 18:46:44 -0700

    main: Remove image and add video for main content. Change language to say happy halloween

--- src/app/Home.tsx
+++ src/app/Home.tsx
@@ -11,23 +11,21 @@ import {
 	BiLogoFacebookCircle,
 	BiMailSend,
 	BiLogoTwitter,
+	BiSolidHeart,
 } from "react-icons/bi";
 
 const Home = () => {
+	const shareable = false;
 	const name = "Jax & Kennedy";
-	const image = "/booImage.jpg";
+	const image = "";
+	const video = "/happy_halloween_2023.MOV";
 
 	return (
 		<div className="text-center">
 			<h1 className="md:text-huge/[120px] text-[110px]/[100px] text-orange mb-8">
 				Booo!
 			</h1>
-			<p className="text-3xl md:text-5xl mb-4">
-				You officially got{" "}
-				<span className="text-orange">{`Boo'd`}</span> by
-				<br />
-				<span className="text-purple">{name}</span>
-			</p>
+
 			{image && (
 				<Image
 					width="800"
@@ -37,6 +35,18 @@ const Home = () => {
 					alt={`${name}'s Booo picture`}
 				/>
 			)}
+			{video && (
+				<video
+					className="inline"
+					width="640"
+					height="480"
+					controls
+					controlsList="nodownload"
+				>
+					<source src={video} type="video/mp4" />
+					Your browser does not support the video tag.
+				</video>
+			)}
 			<div className="flex justify-center gap-x-4">
 				<Ghost className="text-purple animate-[bounce_1s_infinite_0ms]" />
 				<Ghost className="text-orange motion-safe:animate-[bounce_1s_infinite_500ms]" />
@@ -45,37 +55,51 @@ const Home = () => {
 				<Ghost className="text-purple animate-[bounce_1s_infinite_150ms]" />
 				<Ghost className="text-orange motion-safe:animate-[bounce_1s_infinite_800ms]" />
 			</div>
-			<p className="text-4xl md:text-7xl my-6 md:mt-8">
-				Happy Halloween!
+			<p className="text-3xl md:text-5xl mt-8">
+				Happy Halloween from <br />
+				<span className="text-purple">{name}</span>
 			</p>
-			<p className="text-xl mb-4">Share:</p>
-			{typeof window !== "undefined" && (
-				<div className="flex justify-center items-center gap-x-4">
-					<FacebookShareButton
-						title="Check out who Boo'd me!"
-						hashtag="#Booo #HappyHalloween"
-						url={window.location.href}
-					>
-						<BiLogoFacebookCircle
-							className="text-purple"
-							size={64}
-						/>
-					</FacebookShareButton>
-					<TwitterShareButton
-						title="Check out who Boo'd me!"
-						url={window.location.href}
-						hashtags={["Booo", "HappyHalloween"]}
-					>
-						<BiLogoTwitter className="text-purple" size={64} />
-					</TwitterShareButton>
-					<EmailShareButton
-						subject="Check out who Boo'd me!"
-						url={window.location.href}
-					>
-						<BiMailSend className="text-purple" size={64} />
-					</EmailShareButton>
-				</div>
+			{shareable && typeof window !== "undefined" && (
+				<>
+					<p className="text-xl mb-4">Share:</p>
+					<div className="flex justify-center items-center gap-x-4">
+						<FacebookShareButton
+							title="Check out who Boo'd me!"
+							hashtag="#Booo #HappyHalloween"
+							url={window.location.href}
+						>
+							<BiLogoFacebookCircle
+								className="text-purple"
+								size={64}
+							/>
+						</FacebookShareButton>
+						<TwitterShareButton
+							title="Check out who Boo'd me!"
+							url={window.location.href}
+							hashtags={["Booo", "HappyHalloween"]}
+						>
+							<BiLogoTwitter className="text-purple" size={64} />
+						</TwitterShareButton>
+						<EmailShareButton
+							subject="Check out who Boo'd me!"
+							url={window.location.href}
+						>
+							<BiMailSend className="text-purple" size={64} />
+						</EmailShareButton>
+					</div>
+				</>
 			)}
+			<p className="text-xl mt-8">
+				Page created <BiSolidHeart className="inline text-orange" />{" "}
+				love by <br />
+				<a
+					className="hover:text-purple text-orange transition ease-in-out duration-300"
+					href="https://thekirkconcept.com"
+					target="_blank"
+				>
+					The Kirk Concept
+				</a>
+			</p>
 		</div>
 	);
 };
