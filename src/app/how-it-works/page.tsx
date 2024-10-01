ae1512e343e9e3efe9afcb335a8f0c2e90fbec93
christoph-codes <tkcwebdev@gmail.com>
2024-10-01 09:41:31 -0700

    1: Create how it works page with new page template

--- src/app/how-it-works/page.tsx
+++ src/app/how-it-works/page.tsx
@@ -1,9 +1,5 @@
 import HowItWorks from "./HowItWorks";
 
 export default function Page() {
-	return (
-		<main className="flex min-h-screen w-full flex-col p-4 bg-pattern bg-center">
-			<HowItWorks />
-		</main>
-	);
+	return <HowItWorks />;
 }
