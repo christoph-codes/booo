7a53d3165fd93a097486224cff85241c7fbed1a0
christoph-codes <tkcwebdev@gmail.com>
2024-09-30 22:32:40 -0700

    1: Set brand configurations

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -10,13 +10,22 @@ const config: Config = {
 		extend: {
 			colors: {
 				purple: "#3D1AC6",
+				purple_hover: "#281085",
 				orange: "#FF5823",
+				orange_hover: "#CB3506",
+				gray: "#BFBFBF",
+				white: "#FFFFFF",
+				white_hover: "#F2F2F2",
 			},
 			fontSize: {
 				huge: "180px",
 			},
 			backgroundImage: {
 				pattern: "url('/bg.png')",
+			},
+			fontFamily: {
+				sans: ["var(--font-luckiest)"],
+				body: ["var(--font-roboto)"],
 			},
 		},
 	},
