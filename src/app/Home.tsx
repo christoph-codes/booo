0bbc5777e8a6a552aaf3e2691f56394d498cbb8d
christoph-codes <tkcwebdev@gmail.com>
2024-10-01 09:41:13 -0700

    1: Update home page to use new page template

--- src/app/Home.tsx
+++ src/app/Home.tsx
@@ -1,17 +1,9 @@
 "use client";
 
-import {
-	EmailShareButton,
-	FacebookShareButton,
-	TwitterShareButton,
-} from "react-share";
-import {
-	BiLogoFacebookCircle,
-	BiMailSend,
-	BiLogoTwitter,
-} from "react-icons/bi";
 import { Button } from "@/components/Button";
 import { useRouter } from "next/navigation";
+import PageTemplate from "@/templates/PageTemplate";
+import Container from "@/components/Container";
 
 const Home = () => {
 	const router = useRouter();
@@ -27,36 +19,35 @@ const Home = () => {
 		router.push("/create");
 	};
 	return (
-		<div className="text-center space-y-6">
-			<div>
-				<h1 className="md:text-[48px] text-3xl text-white leading-snug">
-					Create your Halloween message and Share it with a video!
-				</h1>
-				<p className="font-medium text-xl md:text-2xl">
-					Create a personalized Booo! page that shows a video and a greeting
-					this Halloween.
-				</p>
-			</div>
-			<div className="flex justify-center items-center gap-x-4">
-				<Button variant="secondary" onClick={handleHowItWorks}>
-					How it Works
-				</Button>
-				<Button onClick={handleCreateBooo}>Create Booo!</Button>
-			</div>
-			{video && (
-				<video
-					className="inline"
-					width="640"
-					height="480"
-					controls
-					controlsList="nodownload"
-					autoPlay
-				>
-					<source src={video} type="video/mp4" />
-					Your browser does not support the video tag.
-				</video>
-			)}
-		</div>
+		<PageTemplate
+			title="Create your Halloween message and Share it with a video!"
+			description="Create a personalized Booo! page that shows a video and a greeting
+						this Halloween."
+		>
+			<Container>
+				<div className="text-center space-y-6">
+					<div className="flex justify-center items-center gap-x-4">
+						<Button variant="secondary" onClick={handleHowItWorks}>
+							How it Works
+						</Button>
+						<Button onClick={handleCreateBooo}>Create Booo!</Button>
+					</div>
+					{video && (
+						<video
+							className="inline"
+							width="640"
+							height="480"
+							controls
+							controlsList="nodownload"
+							autoPlay
+						>
+							<source src={video} type="video/mp4" />
+							Your browser does not support the video tag.
+						</video>
+					)}
+				</div>
+			</Container>
+		</PageTemplate>
 	);
 };
 
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,5 @@
 import Home from "./Home";
 
 export default function Page() {
-	return (
-		<main className="flex min-h-screen w-full flex-col items-center justify-center md:p-36 p-4 bg-pattern bg-center">
-			<Home />
-		</main>
-	);
+	return <Home />;
 }
