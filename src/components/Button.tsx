1f8c82b5c4a138c577a7cd2ab18795684a06c113
christoph-codes <tkcwebdev@gmail.com>
2024-10-02 01:41:21 -0700

    1: Add forward ref to button component and export base button classes for the file upload button

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -4,28 +4,31 @@ import { twMerge } from "tailwind-merge";
 export type ButtonProps = {
 	children: string;
 	className?: string;
-	variant?: "primary" | "secondary" | "ghost";
+	variant?: "primary" | "secondary" | "tertiary" | "ghost";
 };
 
-const baseButtonClasses =
-	"px-4 pt-3 pb-2 rounded-xl text-white hover:text-gray text-center font-sans leading-none text-lg border-2 transition-all ease-in-out duration-300";
+export const baseButtonClasses =
+	"px-4 cursor-pointer pt-3 pb-2 rounded-xl text-white hover:text-gray text-center font-sans leading-none text-lg border-2 transition-all ease-in-out duration-300";
 
 const buttonClasses = {
 	primary:
 		"bg-orange hover:bg-orange_hover border-orange hover:border-orange_hover",
 	secondary: "bg-transparent hover:border-orange_hover border-orange",
+	tertiary:
+		"bg-purple hover:bg-purple_hover border-purple hover:border-purple_hover",
 	ghost: "bg-transparent border-transparent",
 };
 
-export function Button({
-	children,
-	className,
-	variant = "primary",
-	...rest
-}: ButtonProps & ButtonHTMLAttributes<HTMLButtonElement>) {
+import { forwardRef } from "react";
+
+export const Button = forwardRef<
+	HTMLButtonElement,
+	ButtonProps & ButtonHTMLAttributes<HTMLButtonElement>
+>(({ children, className, variant = "primary", ...rest }, ref) => {
 	return (
 		<button
 			{...rest}
+			ref={ref}
 			className={twMerge(
 				baseButtonClasses,
 				buttonClasses[variant] ?? "",
@@ -35,4 +38,6 @@ export function Button({
 			{children}
 		</button>
 	);
-}
+});
+
+Button.displayName = "Button";
