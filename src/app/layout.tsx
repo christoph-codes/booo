4f22a68052aa687842cd8eb2d091fa64c21c145a
christoph-codes <tkcwebdev@gmail.com>
2024-10-17 03:09:58 -0700

    3: Add auth provider to the app layout file

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,31 +1,29 @@
+"use client";
 import Footer from "@/components/Footer";
-import type { Metadata } from "next";
 import { luckiest, roboto } from "./fonts";
 import "./globals.scss";
 import Nav from "@/components/Nav";
 import { SidenavProvider } from "@/context/SidenavProvider";
-
-export const metadata: Metadata = {
-	title: "Booo! 👻",
-	description: "You've Been Booo'd! Check out by who!",
-};
+import AuthProvider from "@/context/AuthProvider";
 
 export default function RootLayout({
-	children,
+  children,
 }: {
-	children: React.ReactNode;
+  children: React.ReactNode;
 }) {
-	return (
-		<html lang="en">
-			<body
-				className={`${roboto.variable} ${luckiest.variable} bg-pattern bg-black bg-center overflow-x-hidden`}
-			>
-				<SidenavProvider>
-					<Nav />
-					{children}
-					<Footer />
-				</SidenavProvider>
-			</body>
-		</html>
-	);
+  return (
+    <html lang="en">
+      <body
+        className={`${roboto.variable} ${luckiest.variable} bg-pattern bg-black bg-center overflow-x-hidden`}
+      >
+        <AuthProvider>
+          <SidenavProvider>
+            <Nav />
+            {children}
+            <Footer />
+          </SidenavProvider>
+        </AuthProvider>
+      </body>
+    </html>
+  );
 }
