5978f36d3c7db6367b986579054e6bbc67e1416d
christoph-codes <tkcwebdev@gmail.com>
2024-10-17 03:13:16 -0700

    3: Move page metadata to home page ssr page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,11 @@
+import { Metadata } from "next";
 import Home from "./Home";
 
+export const metadata: Metadata = {
+  title: "Booo! 👻",
+  description: "Welcome to Booo! 👻",
+};
+
 export default function Page() {
-	return <Home />;
+  return <Home />;
 }
