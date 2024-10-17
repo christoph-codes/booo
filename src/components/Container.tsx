bf806ddcaa07819c55058f5adba869217103d5a8
christoph-codes <tkcwebdev@gmail.com>
2024-10-17 03:12:41 -0700

    3: Ensure custom classnames are applied after container component root styles

--- src/components/Container.tsx
+++ src/components/Container.tsx
@@ -2,27 +2,27 @@ import { ReactNode } from "react";
 import { twMerge } from "tailwind-merge";
 
 export type ContainerProps = {
-	size?: "sm" | "md" | "lg";
-	children: ReactNode;
-	className?: string;
+  size?: "sm" | "md" | "lg";
+  children: ReactNode;
+  className?: string;
 };
 
 const Container = ({ children, size, className }: ContainerProps) => {
-	return (
-		<div
-			className={twMerge(
-				"container mx-auto px-2 md:px-6",
-				className,
-				size === "sm"
-					? "max-w-md"
-					: size === "md"
-					? "max-w-lg"
-					: size === "lg" && "max-w-5xl"
-			)}
-		>
-			{children}
-		</div>
-	);
+  return (
+    <div
+      className={twMerge(
+        "container mx-auto px-2 md:px-6",
+        size === "sm"
+          ? "max-w-md"
+          : size === "md"
+          ? "max-w-lg"
+          : size === "lg" && "max-w-5xl",
+        className
+      )}
+    >
+      {children}
+    </div>
+  );
 };
 
 export default Container;
