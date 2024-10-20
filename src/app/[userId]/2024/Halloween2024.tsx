01665f23bcb2c6fbfc5b88ffb56d33c32a003e3c
christoph-codes <tkcwebdev@gmail.com>
2024-10-20 00:35:08 -0700

    4: Add sharing capabilities on the profile page

--- src/app/[userId]/2024/Halloween2024.tsx
+++ src/app/[userId]/2024/Halloween2024.tsx
@@ -1,5 +1,17 @@
+"use client";
 import BouncingGhosts from "@/components/BouncingGhosts";
+import {
+	baseButtonClasses,
+	Button,
+	buttonSizes,
+	buttonVariants,
+} from "@/components/Button";
+import Modal from "@/components/Modal";
 import Video from "@/components/Video";
+import useModal from "@/hooks/useModal";
+import Image from "next/image";
+import Link from "next/link";
+import { MouseEvent } from "react";
 import {
 	BiLogoFacebookCircle,
 	BiLogoTwitter,
@@ -10,6 +22,7 @@ import {
 	FacebookShareButton,
 	TwitterShareButton,
 } from "react-share";
+import { twMerge } from "tailwind-merge";
 
 export type BoooData = {
 	userId: string;
@@ -37,6 +50,63 @@ const Holiday2024 = ({ booo }: Halloween2024Props) => {
 		printWindow?.print();
 	};
 	return (
+		<>
+			<div className="text-center min-h-[70vh] flex flex-col items-center justify-center">
+				<div className="max-w-3xl mx-auto mt-16">
+					<h1 className="text-white mb-1">
+						{booo.message} from{" "}
+						{booo.name ? (
+							<>
+								<br />
+								<span className="text-orange">{booo.name}!</span>
+							</>
+						) : (
+							"Booo!"
+						)}
+					</h1>
+					{booo.video && <Video autoPlay url={booo.video} />}
+					<BouncingGhosts />
+					<Button
+						className="mt-8 text-3xl -pb-2"
+						size="lg"
+						variant="secondary"
+						onClick={handleShareButtonClick}
+					>
+						Share
+					</Button>
+				</div>
+			</div>
+			<Modal
+				title="Share your Booo!"
+				description="Download the QR code to share your Booo with friends and family or share directly to social media!"
+				isOpen={saveQRCodeModal.isOpen}
+				onClose={saveQRCodeModal.close}
+				cancel={false}
+				success={false}
+			>
+				<div className="flex flex-col items-center mt-6 gap-y-4">
+					<p>Share to social media</p>
+					<div className="flex justify-center items-center gap-x-8 pb-8 border-b border-b-gray_dark w-full">
+						<FacebookShareButton
+							hashtag="Check out who Boo'd me! #Booo #HappyHalloween"
+							url={boooURL}
+						>
+							<BiLogoFacebookCircle className="text-purple" size={48} />
+						</FacebookShareButton>
+						<TwitterShareButton
+							title="Check out who Boo'd me!"
+							url={boooURL}
+							hashtags={["Booo", "HappyHalloween"]}
+						>
+							<BiLogoTwitter className="text-purple" size={48} />
+						</TwitterShareButton>
+						<Link
+							href={`mailto:cjones@thekirkconcept.com?subject=Check out who Boo'd you!&body=You just got Boo'd by ${booo.name}! Check it out at ${boooURL}`}
+							className={twMerge(buttonVariants["ghost"], "cursor-pointer")}
+						>
+							<BiMailSend className="text-purple" size={48} />
+						</Link>
+					</div>
 					{booo.qrCodeImage && (
 						<>
 							<p>or save the QR code</p>
@@ -68,6 +138,11 @@ const Holiday2024 = ({ booo }: Halloween2024Props) => {
 									Print
 								</Button>
 							</div>
+						</>
+					)}
+				</div>
+			</Modal>
+		</>
 	);
 };
 
