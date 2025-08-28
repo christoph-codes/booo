67f7b3ca6049f41601652a37c5bf21af50cfca21
Christopher Jones <tkcwebdev@gmail.com>
2025-08-28 12:42:28 -0700

    9: Fix build errors with page props and react-share not being compatible with react 19

--- src/app/[userId]/2024/page.tsx
+++ src/app/[userId]/2024/page.tsx
@@ -2,15 +2,19 @@ import { collection, getDocs, query, where } from "firebase/firestore";
 import Halloween2024, { BoooData } from "./Halloween2024";
 import { db } from "@/utils/firebase";
 
-export default async function Page({ params }: { params: { userId: string } }) {
-	const userId = params.userId;
+export default async function Page({
+	params,
+}: {
+	params: Promise<{ userId: string }>;
+}) {
+	const { userId } = await params;
 	const year = "2024";
 
 	const collectionRef = collection(db, "booos");
 	const q = query(
 		collectionRef,
 		where("userId", "==", userId),
-		where("year", "==", year)
+		where("year", "==", year),
 	);
 
 	const querySnapshot = await getDocs(q);
